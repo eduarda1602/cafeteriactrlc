113eee0f2f7746e26bc958228919ea1c1a8afc63
eduarda1602 <mariaeduardalopespinto2@gmail.com>
2026-03-12 16:55:48 -0500

    dudis

---
+++ script.js
@@ -0,0 +1,72 @@
+const cafe =[
+    {
+        nome:"espresso",
+        descricao:"cafe concentrado y aromatico",
+        preco:"$3.50",
+        img:"assents/espresso.jpg",
+    
+    }
+,
+    {
+        nome:"cappuccino",
+        descricao:"espresso con leche vaporizada y espuma",
+        preco:"$4.50",
+        img:"assents/cappuccino.jpg",
+
+    }
+,
+    {
+
+        nome:"cafe helado",
+        descricao:"refrescante cafe frio con hielo",
+        preco:"$5.00",
+        img:"assents/cafehelado.jpg",
+    }
+       
+    
+   
+]
+
+const confeitaria=[
+    {
+        nome:"pastel de chocolate",
+        descricao:"delicioso pastel de chocolate casero",
+        preco:"$4.00",
+        img:"assents/pasteldechocolate.jpg",
+    
+    }
+,
+    {
+        nome:"croissant",
+        descricao:"croissant frances racien horneado",
+        preco:"$3.00",
+        img:"assents/croissant.jpg",
+
+    }
+,
+    {
+
+        nome:"muffin de arandanos",
+        descricao:"esponjoso muffin con arandanos frescos",
+        preco:"$3.50",
+        img:"assents/muffin.jpg",
+    }
+       
+    
+   
+]
+
+
+const comida =[
+    {
+        nome: "Sándwich de Pavo",
+        descricao: "Sándwich fresco con pavo y vegetales",
+        preco:"$6.50",
+        img: "assents/sandwichdepavo.jpg"
+    }
+]
+
+
+
+
+
